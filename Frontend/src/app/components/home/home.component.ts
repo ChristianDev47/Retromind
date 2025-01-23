cb53451eafbae80cfcac274fe93903bfb5fc84d6
Christian Delgado <christiands.dev@gmail.com>
2025-01-23 19:51:29 -0400

    Update

--- Frontend/src/app/components/home/home.component.ts
+++ Frontend/src/app/components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HeroComponent } from '../hero/hero.component';
+import { PrincipalComponent } from '../principal/principal.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent],
+  imports: [HeroComponent, PrincipalComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
